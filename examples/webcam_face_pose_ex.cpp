da685c5d092d5067a01aafae12b30218090156bc
efun100 <1483586698@qq.com>
2019-04-05 16:14:53 +0800

    remove gui_core

--- examples/webcam_face_pose_ex.cpp
+++ examples/webcam_face_pose_ex.cpp
@@ -31,7 +31,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <dlib/image_processing/frontal_face_detector.h>
-#include <dlib/image_processing.h>
+// #include <dlib/image_processing.h>
 #include "model_utils.hpp"
 
 using namespace dlib;
